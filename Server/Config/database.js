794b0c7ea6d2172876820118b856f0be56f0432a
ShakeerMirja <144615063+ShakeerMirja@users.noreply.github.com>
2025-12-30 00:50:22 +0530

    Create database.js

---
+++ Server/Config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+exports.connectdb = () => {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => console.log(error.message));
+}
